7910e309c385739e9b3481e34eed988eabb0bee1
“Cesar <sgroja6@gmail.com>
2022-05-10 15:37:30 -0500

    ADD project MS

---
+++ myfirstms/src/main/java/com/nttdata/bootcamp/myfirstms/MyfirstmsApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.myfirstms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstmsApplication.class, args);
+	}
+
+}
