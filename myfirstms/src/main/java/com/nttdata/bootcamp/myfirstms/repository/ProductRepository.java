5b299ba7f6f0c2c8744903593ea8e1cf0f11bd53
“Cesar <sgroja6@gmail.com>
2022-05-11 18:57:55 -0500

    Update ProductRepository

---
+++ myfirstms/src/main/java/com/nttdata/bootcamp/myfirstms/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.nttdata.bootcamp.myfirstms.repository;
+
+import com.nttdata.bootcamp.myfirstms.model.dto.Product;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository <Product, String>{
+    
+}
